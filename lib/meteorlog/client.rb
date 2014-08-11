b012a28f69441118ff7383a54d579a80b16e1f55
Genki Sugawara <sugawara@cookpad.com>
2014-08-11 17:58:30 +0900

    Fix client

--- lib/meteorlog/client.rb
+++ lib/meteorlog/client.rb
@@ -11,4 +11,26 @@ class Meteorlog::Client
     exported = Meteorlog::Exporter.export(@cloud_watch_logs, @options.merge(opts))
     Meteorlog::DSL.convert(exported, @options.merge(opts))
   end
+
+  def apply(file)
+    walk(file)
+  end
+
+  private
+
+  def walk(file)
+    dsl = load_file(file)
+  end
+
+  def load_file(file)
+    if file.kind_of?(String)
+      open(file) do |f|
+        Meteorlog::DSL.parse(f.read, file)
+      end
+    elsif file.respond_to?(:read)
+      Meteorlog::DSL.parse(file.read, file.path)
+    else
+      raise TypeError, "can't convert #{file} into File"
+    end
+  end
 end
